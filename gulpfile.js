d3eb57d24ca6c62a7ec2574ab0668f85e5916b7e
Dima Kovalchuk <kovaldn@gmail.com>
2014-12-25 23:26:01 +0300

    Happy New Year! PS - fix clean

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,41 @@
 
 var gulp = require('gulp'),
 		connect = require('gulp-connect'),
-		opn = require('opn');
+		opn = require('opn'),
+		wiredep = require('wiredep').stream,
+		useref = require('gulp-useref'),
+    gulpif = require('gulp-if'),
+    uglify = require('gulp-uglify'),
+    minifyCss = require('gulp-minify-css'),
+    clean = require('clean');
 
+// Очистка
+// gulp.task('clean', function () {
+// 	return gulp.src('dist').pipe(clean());
+// });
+
+// Сборка проекта
+gulp.task('dist', function () {
+    var assets = useref.assets();
+    return gulp.src('app/*.html')
+        .pipe(assets)
+        .pipe(gulpif('*.js', uglify()))
+        .pipe(gulpif('*.css', minifyCss()))
+        .pipe(assets.restore())
+        .pipe(useref())
+        .pipe(gulp.dest('dist'));
+});
+
+// Work with bower
+gulp.task('wiredep', function () {
+  gulp.src('app/*.html')
+    .pipe(wiredep({
+      directory: 'app/bower_components'
+    }))
+    .pipe(gulp.dest('app'));
+});
+
+// Server
 gulp.task('connect', function() {
   connect.server({
     root: 'app',
@@ -35,6 +68,7 @@ gulp.task('watch', function (){
 	gulp.watch(['app/*.html'], ['html']);
 	gulp.watch(['app/css/*.css'], ['css']);
 	gulp.watch(['app/js/*.js'], ['js']);
+	gulp.watch('bower.json', ['wiredep']);
 })
 
 // Default
